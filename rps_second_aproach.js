4283ad31424ecdca49be64d0494ead24a7540970
ErickDaniCB <erickdani10@hotmail.com>
2023-05-18 14:30:28 -0600

    Add an adapted version of rps.js's singleRound function to iterate the game.

--- rps_second_aproach.js
+++ rps_second_aproach.js
@@ -6,20 +6,56 @@ In the rock, paper, scissors game there's only two players
 so we need to compare both inputs and add the result to their current score.
 */
 
-let playerPlayRaw = "Rock";
+let playerPlayRaw = "scissors";
 let playerPlay = playerPlayRaw.toUpperCase();
-let computerPlay = "";
+let computerPlay = ""
 let playerScore = 0;
 let computerScore = 0;
 
 //Now we need a way to generate the computer play to assign to it's correspondent variable.
 
-function getComputerChoice () {
+function getComputerChoice(){
     let possiblePlays = ["ROCK", "PAPER", "SCISSORS"];
     let guess = Math.floor(Math.random() * possiblePlays.length);
     return computerPlay = possiblePlays[guess]; 
 }
 
-console.log(getComputerChoice());
-console.log(computerPlay); //Our variable is receiving the random value as intended!
+
+//Next we need to compare our play inputs in order to see who's the winner of the round.
+
+function singleRound(playerPlay, computerPlay){
+    
+    getComputerChoice();
+
+    if (playerPlay === "ROCK"){
+        if (computerPlay === "SCISSORS"){
+            return `You win! ${playerPlay} beats ${computerPlay}.`;
+        }
+        else if (computerPlay === "PAPER"){
+            return `You lose! ${computerPlay} beats ${playerPlay}.`;
+        } 
+        else return `It's a tie!`;
+    }
+    
+    else if (playerPlay === "PAPER"){
+        if (computerPlay === "ROCK"){
+            return `You win! ${playerPlay} beats ${computerPlay}.`;
+        }
+        else if (computerPlay === "SCISSORS"){
+            return `You lose! ${computerPlay} beats ${playerPlay}.`;
+        }
+        else return `It's a tie!`;
+    }
+
+    else if (playerPlay === "SCISSORS"){
+        if (computerPlay === "PAPER"){
+            return `You win! ${playerPlay} beats ${computerPlay}.`;
+        }
+        else if (computerPlay === "ROCK"){
+            return `You lose! ${computerPlay} beats ${playerPlay}.`;
+        }
+        else return `It's a tie!`;
+    } 
+    
+}
 
