907b4778fc639b153420ade39d376ee69e270c84
ErickDaniCB <erickdani10@hotmail.com>
2023-05-18 17:49:33 -0600

    Implement function game() that plays multiple rounds of the game

--- rps_second_aproach.js
+++ rps_second_aproach.js
@@ -6,27 +6,30 @@ In the rock, paper, scissors game there's only two players
 so we need to compare both inputs and add the result to their current score.
 */
 
-let playerPlayRaw = "scissors";
-let playerPlay = playerPlayRaw.toUpperCase();
+let possiblePlays = ["ROCK", "PAPER", "SCISSORS"];
+let playerPlayRaw = "";
+let playerPlay = "";
 let computerPlay = ""
-let playerScore = 0;
-let computerScore = 0;
+//let playerScore = 0;
+//let computerScore = 0;
 
 //Now we need a way to generate the computer play to assign to it's correspondent variable.
 
 function getComputerChoice(){
-    let possiblePlays = ["ROCK", "PAPER", "SCISSORS"];
     let guess = Math.floor(Math.random() * possiblePlays.length);
     return computerPlay = possiblePlays[guess]; 
 }
 
+function playerPrompt() {
+    playerPlayRaw = prompt("Make your play");
+    console.log(`Your play is ${playerPlayRaw}!`)
+    return playerPlay = playerPlayRaw.toUpperCase()
+}
 
 //Next we need to compare our play inputs in order to see who's the winner of the round.
 
 function singleRound(playerPlay, computerPlay){
     
-    getComputerChoice();
-
     if (playerPlay === "ROCK"){
         if (computerPlay === "SCISSORS"){
             return `You win! ${playerPlay} beats ${computerPlay}.`;
@@ -55,7 +58,27 @@ function singleRound(playerPlay, computerPlay){
             return `You lose! ${computerPlay} beats ${playerPlay}.`;
         }
         else return `It's a tie!`;
-    } 
+    } else alert("Invalid play!");
     
 }
 
+function game(){
+    for (let i = 1; i <= 5; i++){
+        switch (i) {
+            case 1:
+                console.log("Round 1:")
+                playerPrompt();
+                getComputerChoice();
+                console.log(singleRound(playerPlay, computerPlay));
+                break;
+            case 2:
+                console.log("Round 2:")
+                playerPrompt();
+                getComputerChoice();
+                console.log(singleRound(playerPlay, computerPlay));
+                break;
+        }
+    }
+}
+
+game();
