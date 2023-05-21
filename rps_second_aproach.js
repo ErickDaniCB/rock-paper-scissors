d969058b49f7910887705f251b478b34709a313c
ErickDaniCB <erickdani10@hotmail.com>
2023-05-20 18:10:26 -0600

    Add scoring system and rebuild game() to be a best-of-five

--- rps_second_aproach.js
+++ rps_second_aproach.js
@@ -9,13 +9,13 @@ so we need to compare both inputs and add the result to their current score.
 let possiblePlays = ["ROCK", "PAPER", "SCISSORS"];
 let playerPlayRaw = "";
 let playerPlay = "";
-//let computerPlay = ""
-//let playerScore = 0;
-//let computerScore = 0;
-
+let playerScore = 0;
+let computerScore = 0;
+let singleRoundResult; 
+let i;
 //Now we need a way to generate the computer play to assign to it's correspondent variable.
 
-function getComputerChoice(){
+function getComputerChoice() {
     let guess = Math.floor(Math.random() * possiblePlays.length);
     return computerPlay = possiblePlays[guess]; 
 }
@@ -28,63 +28,113 @@ function playerPrompt() {
 
 //Next we need to compare play inputs in order to see who's the winner of the round.
 
-function singleRound(playerPlay, computerPlay){
-    
-    if (playerPlay === "ROCK"){
-        if (computerPlay === "SCISSORS"){
-            return `You win! ${playerPlay} beats ${computerPlay}.`;
-        }
-        else if (computerPlay === "PAPER"){
-            return `You lose! ${computerPlay} beats ${playerPlay}.`;
-        } 
-        else return `It's a tie!`;
+function singleRound(playerPlay, computerPlay) {
+
+    if (playerPlay !== "ROCK" && playerPlay !== "PAPER" && playerPlay !== "SCISSORS"){
+        return 3 // 3 = Invalid!
     }
     
-    else if (playerPlay === "PAPER"){
-        if (computerPlay === "ROCK"){
-            return `You win! ${playerPlay} beats ${computerPlay}.`;
+    else if (playerPlay === "ROCK") {
+        if (computerPlay === "SCISSORS") {
+            return 1 // 1 = Win!
         }
-        else if (computerPlay === "SCISSORS"){
-            return `You lose! ${computerPlay} beats ${playerPlay}.`;
+        else if (computerPlay === "PAPER") {
+            return 0 // 0 = Lose!
+        } 
+        else return 2 // 2 = Tie!;
+    }
+    
+    else if (playerPlay === "PAPER") {
+        if (computerPlay === "ROCK") {
+            return 1 // 1 = Win!
         }
-        else return `It's a tie!`;
+        else if (computerPlay === "SCISSORS") {
+            return 0 // 0 = Lose!
+        }
+        else return 2 // 2 = Tie!;
     }
 
-    else if (playerPlay === "SCISSORS"){
-        if (computerPlay === "PAPER"){
-            return `You win! ${playerPlay} beats ${computerPlay}.`;
+    else if (playerPlay === "SCISSORS") {
+        if (computerPlay === "PAPER") {
+            return 1 // 1 = Win!
         }
-        else if (computerPlay === "ROCK"){
-            return `You lose! ${computerPlay} beats ${playerPlay}.`;
+        else if (computerPlay === "ROCK") {
+            return 0 // 0 = Lose!
         }
-        else return `It's a tie!`;
-    } else alert("Invalid play!");
+        else return 2 // 2 = Tie!;
+    } 
     
+}
+
+//We need a function to add score to our opponents!
+
+function scoring (){
+    switch (singleRoundResult){
+        case 0: 
+            computerScore += 1;
+            console.log(`You lose! ${computerPlay} beats ${playerPlay}.`)
+        break;
+        case 1:
+            playerScore += 1;
+            console.log(`You win! ${playerPlay} beats ${computerPlay}.`)
+        break;
+        case 2:
+            i -= 1
+            console.log(`Tie!`);
+        break;
+        case 3:
+            i -= 1;
+            console.log(`Invalid play!`);
+        break;
+    }
+
+    console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
 }
 
 //The function below let us play the game 5 times in a row. 
 
-function game(){
-    for (let i = 1; i <= 5; i++){
-        console.log(`Round ${i}!`);
-        let computerPlay = getComputerChoice();
-        playerPrompt();
-        switch (i) {
-            case 1:
-                console.log(singleRound(playerPlay, computerPlay));
-                break;
-            case 2:
-                console.log(singleRound(playerPlay, computerPlay));
-                break;
-            case 3:
-                console.log(singleRound(playerPlay, computerPlay));
-                break;
-            case 4:
-                console.log(singleRound(playerPlay, computerPlay));
-                break;
-            case 5:
-                console.log(singleRound(playerPlay, computerPlay));
-                break;
+function game() {
+   
+    playerScore = 0;
+    computerScore = 0;
+    i = 1;
+
+    for (i; i <= 5; i++) {
+        if (playerScore === 3) {
+            console.log(`Congratulations, you won the match!`)
+            i = 6;
+        } 
+        else if (computerScore === 3) {
+            console.log(`Sorry, you lost the match!`)
+            i = 6;
+        }
+        else {
+            console.log(`Round ${i}!`);
+            let computerPlay = getComputerChoice();
+            playerPrompt();
+            switch (i) {
+                case 1:
+                    singleRoundResult = singleRound(playerPlay, computerPlay);
+                    scoring();
+                    break;
+                case 2:
+                    singleRoundResult = singleRound(playerPlay, computerPlay);
+                    scoring();
+                    break;
+                case 3:
+                    singleRoundResult = singleRound(playerPlay, computerPlay);
+                    scoring();
+                    break;
+                case 4:
+                    singleRoundResult = singleRound(playerPlay, computerPlay);
+                    scoring();
+                    break;
+                case 5:
+                    singleRoundResult = singleRound(playerPlay, computerPlay);
+                    scoring();
+                    break;
+            }
         }
     }
+
 }
